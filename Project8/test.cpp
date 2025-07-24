2d8b5c2a2d6051e847fe09c2bf305d5c98329535
dooyeun <ghkdendus330@gmail.com>
2025-07-24 14:03:50 +0900

    테스트케이스 리팩토링 완료

--- Project8/test.cpp
+++ Project8/test.cpp
@@ -5,17 +5,29 @@
 #include "sunday_booking_scheduler.cpp"
 #include "monday_booking_scheduler.cpp"
 #include "testable_booking_scheduler.cpp"
+
 using namespace testing;
+
+class MockCustomer : public Customer {
+public:
+	MOCK_METHOD(string, getEmail, (), (override));
+};
+
 class BookingItem : public Test {
 protected:
 	void SetUp() override {
 		NOT_ON_THE_HOUR = getTime(2021, 3, 26, 9, 5);
 		ON_THE_HOUR = getTime(2021, 3, 26, 9, 0);
-		ON_SUNDAY = getTime(2021, 3, 28, 17, 0);
-		ON_MONDAY = getTime(2024, 6, 3, 17, 0);
+		SUNDAY_ON_THE_HOUR = getTime(2021, 3, 28, 17, 0);
+		MONDAY_ON_THE_HOUR = getTime(2024, 6, 3, 17, 0);
 
 		bookingScheduler.setSmsSender(&testableSmsSender);
 		bookingScheduler.setMailSender(&testableMailSender);
+		
+		EXPECT_CALL(CUSTOMER, getEmail)
+			.WillRepeatedly(testing::Return(""));
+		EXPECT_CALL(CUSTOMER_WITH_MAIL, getEmail)
+			.WillRepeatedly(testing::Return("test@test.com"));
 	}
 public:
 	tm getTime(int year, int mon, int day, int hour, int min) {
@@ -32,16 +44,18 @@ public:
 
 	tm NOT_ON_THE_HOUR;
 	tm ON_THE_HOUR;
-	tm ON_SUNDAY;
-	tm ON_MONDAY;
-	Customer CUSTOMER{ "Fake name", "010-1234-5678" };
-	Customer CUSTOMER_WITH_MAIL{ "Fake Name", "010-1234-5678", "test@test.com" };
+	tm SUNDAY_ON_THE_HOUR;
+	tm MONDAY_ON_THE_HOUR;
+	MockCustomer CUSTOMER;
+	MockCustomer CUSTOMER_WITH_MAIL;
+	//Customer CUSTOMER{ "Fake name", "010-1234-5678" };
+	//Customer CUSTOMER_WITH_MAIL{ "Fake Name", "010-1234-5678", "test@test.com" };
 	const int UNDER_CAPACITY = 1;
 	const int CAPACITY_PER_HOUR = 3;
 
 	BookingScheduler bookingScheduler{ CAPACITY_PER_HOUR };
-	TestableSmsSender testableSmsSender;
-	TestableMailSender testableMailSender;
+	NiceMock<TestableSmsSender> testableSmsSender;
+	NiceMock<TestableMailSender> testableMailSender;
 };
 
 TEST_F(BookingItem, 예약은정시에만가능하다정시가아닌경우예약불가) {
@@ -96,34 +110,37 @@ TEST_F(BookingItem, 시간대별인원제한이있다같은시간대가다르면Capacity차있어도스케
 TEST_F(BookingItem, 예약완료시SMS는무조건발송) {
 	//arrange
 	Schedule* schedule = new Schedule{ ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER };
-	//act
+	//act, assert
+	EXPECT_CALL(testableSmsSender, send(schedule))
+		.Times(1);
 	bookingScheduler.addSchedule(schedule);
-	//assert
-	EXPECT_EQ(true, testableSmsSender.isSendMethodIsCalled());
 }
 
 TEST_F(BookingItem, 이메일이없는경우에는이메일미발송) {
 	//arrange
 	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
-	//act
+	//act, assert
+	EXPECT_CALL(testableMailSender, sendMail(schedule))
+		.Times(0);
 	bookingScheduler.addSchedule(schedule);
-	//assert
-	EXPECT_EQ(0, testableMailSender.getCountSendMailMethodIsCalled());
 }
 
 TEST_F(BookingItem, 이메일이있는경우에는이메일발송) {
 	//arrange
 	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL };
-	//act
+	//act, assert
+	EXPECT_CALL(testableMailSender, sendMail(schedule))
+		.Times(1);
 	bookingScheduler.addSchedule(schedule);
-	//assert
-	EXPECT_EQ(1, testableMailSender.getCountSendMailMethodIsCalled());
 }
 
 
 TEST_F(BookingItem, 현재날짜가일요일인경우예약불가예외처리) {
 	//arrange
-	BookingScheduler* bookingScheduler = new TestableBookingScheduler{ CAPACITY_PER_HOUR, ON_SUNDAY };
+	TestableBookingScheduler mockScheduler{ CAPACITY_PER_HOUR };
+	EXPECT_CALL(mockScheduler, getNow)
+		.WillRepeatedly(testing::Return(mktime(&SUNDAY_ON_THE_HOUR)));
+	BookingScheduler* bookingScheduler = &mockScheduler;
 	try {
 		//act
 		Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL };
@@ -138,7 +155,11 @@ TEST_F(BookingItem, 현재날짜가일요일인경우예약불가예외처리) {
 
 TEST_F(BookingItem, 현재날짜가일요일이아닌경우예약가능) {
 	//arrange
-	BookingScheduler* bookingScheduler = new TestableBookingScheduler{ CAPACITY_PER_HOUR, ON_MONDAY };
+	TestableBookingScheduler mockScheduler{ CAPACITY_PER_HOUR };
+	EXPECT_CALL(mockScheduler, getNow)
+		.WillRepeatedly(testing::Return(mktime(&MONDAY_ON_THE_HOUR)));
+
+	BookingScheduler* bookingScheduler = &mockScheduler;
 	//act
 	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL };
 	bookingScheduler->addSchedule(schedule);
